fbc71dc04844db5c3d3c21535d04a3de1d9d6236
Miłosz Góralczyk <goralczyk.milosz2k@gmail.com>
2024-04-10 19:00:54 +0200

    work in progress

--- ttrpgbackend/api/views.py
+++ ttrpgbackend/api/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from .models import Character
 
 # Create your views here.
 
 def home(request):
     return render(request, "home.html")
+
+def characters(request):
+    chars = Character.objects.all()
+    return render(request, "characters.html", {"characters": chars})
