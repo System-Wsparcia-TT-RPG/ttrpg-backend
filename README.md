be224d030f2f1b01000fa79c57ab65f476dbac6e
ForNeus57 <dominikbreksa@gmail.com>
2024-04-11 11:18:17 +0200

    Final touch in regard to environmental variables.

--- README.md
+++ README.md
@@ -7,13 +7,17 @@
 #### Docker
 
 1. Build container image:
-	```bash
-	docker build . -t tt-rpg-backend:latest
-	```
+```bash
+docker build . -t tt-rpg-backend:latest
+```
 2. Start container:
-	```bash
-	docker run -p 8000:8000 --name tt-rpg-backend tt-rpg-backend:latest
-	```
+```bash
+docker run -p 8000:8000 --name tt-rpg-backend \
+	-e DJANGO_SUPERUSER_USERNAME="..." \
+	-e DJANGO_SUPERUSER_PASSWORD="..." \
+	-e DJANGO_SUPERUSER_EMAIL="..." \
+	tt-rpg-backend:latest
+```
 
 3. Access the API at `http://localhost:8000`
 
--- ttrpgbackend/ttrpgbackend/settings.py
+++ ttrpgbackend/ttrpgbackend/settings.py
@@ -27,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(getenv("SECRET_KEY"))
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(getenv("DEBUG")) == "1"
+DEBUG = bool(getenv("DEBUG") or 0)
 
-ALLOWED_HOSTS = str(getenv("DJANGO_ALLOWED_HOSTS")).split(' ')
+ALLOWED_HOSTS = str(getenv("DJANGO_ALLOWED_HOSTS") or '').split(' ')
 
 # Application definition
 
