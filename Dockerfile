07ebe2922db30b441cf85e27a1962fc45d5483c4
ForNeus57 <dominikbreksa@gmail.com>
2024-04-11 10:52:07 +0200

    Speed up of Dockerfile image building

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,15 @@ FROM python:3.12.2
 
 WORKDIR /app
 
+EXPOSE 8000
+
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-EXPOSE 8000
-
-COPY . .
+COPY ./requirements.txt ./requirements.txt
 RUN pip install --upgrade pip && pip install -r ./requirements.txt
 
-CMD ["python", "ttrpgbackend/manage.py", "runserver", "0.0.0.0:8000"]
+COPY . .
+WORKDIR /app/ttrpgbackend
+
+CMD ["python", "./manage.py", "runserver", "0.0.0.0:8000"]
--- ttrpgbackend/ttrpgbackend/settings.py
+++ ttrpgbackend/ttrpgbackend/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from os import getenv
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv('./.env.dev')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u^8%t=rg_!q+nrq#0(te1gotq#m*2^&)0cy27+6n)0f)7*39up'
+SECRET_KEY = str(getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(getenv("DEBUG")) == "1"
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = str(getenv("DJANGO_ALLOWED_HOSTS")).split(' ')
 
 # Application definition
 
