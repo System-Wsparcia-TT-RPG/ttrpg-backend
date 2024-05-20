a66e182b829025cfcc2d8a78b6c729fc490ce783
ForNeus57 <dominikbreksa@gmail.com>
2024-05-20 17:35:09 +0200

    Removed double default index page.

--- src/web/api/urls.py
+++ src/web/api/urls.py
@@ -5,10 +5,9 @@ urlpatterns = [
     path('character/all/', views.CharacterView.Get.as_view()),
     path("character/<int:character_id>/", views.CharacterView.GetId.as_view()),
     path('character/create/', views.CharacterView.Post.as_view()),
-    # path('character/update/<int:character_id>/', views.CharacterView.Put.as_view()),
-    # path('character/delete/<int:character_id>/', views.CharacterView.Delete.as_view()),
+
     path('race/all/', views.RaceView.GetRaceEnum.as_view()),
+
     path('spell/all/', views.SpellView.Get.as_view()),
     path('spell/create/', views.SpellView.Post.as_view()),
-    path('', views.Index.as_view()),
 ]
